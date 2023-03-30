fac96b11ef2eb2c0b8417955e13c83c4a3f97372
An Ji Hwan <angeljihwan@gmail.com>
2023-03-30 10:15:39 +0900

    Altered minting logics
    Minters will now receive authorities that are used to mint later on. Party mint will not mint all cards at once, instead will give authorities to the mentioned employees. Changed ERC721Enumerable to ERC721EnumerableUpgradeable

--- contracts/BusinessCardBase.sol
+++ contracts/BusinessCardBase.sol
@@ -8,6 +8,10 @@ import "./access/Ownable.sol";
 error BusinessCardBase__NotFirstMint();
 error BusinessCardBase__InvalidString();
 error BusinessCardBase__InvalidETHAmountSent();
+error BusinessCardBase__NotMintable();
+error BusinessCardBase__NotStaked();
+error BusinessCardBase__ExceededPeople();
+error BusinessCardBase__InvalidArrayCount();
 
 contract BusinessCardBase is ERC721EnumerableUpgradeable {
     enum CardType {
@@ -18,14 +22,19 @@ contract BusinessCardBase is ERC721EnumerableUpgradeable {
     Card[] private cards;
     uint32 internal constant MAX_CARDS = 1000;
     uint32 internal constant MAX_FIRST_MINT = 10;
+    uint32 constant MAX_EMPLOYEES = 10;
+    uint32 constant MAX_DECIMALS = 10 ^ 18;
+    uint256 internal stakePrice;
     uint256 internal firstMintPrice;
     uint256 internal mintPrice;
+    uint256 constant STAKE_TIME = 180 days;
 
     // Mappings
     mapping(address => bool) internal firstMinted;
-    mapping(address => uint256) internal addressToCardCount;
-    mapping(uint256 => address) internal cardToAddress;
-    mapping(address => uint256) internal addressToMintableAmount;
+    mapping(address => mapping(address => uint256)) internal addressToDivisionToMintable;
+    mapping(address => uint256) internal stakedTime;
+    mapping(address => bool) internal successfullyStaked;
+    mapping(address => string) internal addressToCompanyName;
 
     struct Card {
         string name;
@@ -34,63 +43,126 @@ contract BusinessCardBase is ERC721EnumerableUpgradeable {
         string company;
         CardType cardType;
         uint256 valueDesired;
+        address owner;
     }
 
     // Events
-    event CardCreated(uint256 indexed id, string name, string email, string phone, string company, uint256 valueDesired);
+    event CardCreated(
+        uint256 indexed id,
+        string name,
+        string email,
+        string phone,
+        string company,
+        uint256 valueDesired,
+        address owner
+    );
     event CardTransfer(uint256 indexed id, address indexed from, address indexed to);
+    event Stake(address indexed staker, uint256 amount, string companyName);
+    event PartyMint(address indexed company, address[] employeeAddresses, uint32 employeeCount);
 
     // Functions
+    modifier isMintable() {
+        if (addressToDivisionToMintable[msg.sender][msg.sender] <= 0) {
+            revert BusinessCardBase__NotMintable();
+        }
+        _;
+    }
 
-    function __BusinessCardBase__init(uint256 _firstMintPrice, uint256 _mintPrice) internal {
+    modifier onlyStaked() {
+        if (successfullyStaked[msg.sender] == false) {
+            revert BusinessCardBase__NotStaked();
+        }
+
+        _;
+    }
+
+    function __BusinessCardBase__init(uint256 _firstMintPrice, uint256 _mintPrice, uint256 _stakePrice) internal {
         __ERC721_init("BusinessCardBase", "BC");
         firstMintPrice = _firstMintPrice;
         mintPrice = _mintPrice;
+        stakePrice = _stakePrice;
     }
 
-    function mint(
-        string memory _name,
-        string memory _email,
-        string memory _phone,
-        string memory _company,
-        uint256 valueDesired
-    ) public payable returns (bool success) {
-        if (
-            bytes(_name).length == 0 || bytes(_email).length == 0 || bytes(_phone).length == 0 || bytes(_company).length == 0 || 
-        ) {
-            revert BusinessCardBase__InvalidString();
-        }
-
-        uint256 _id;
-
+    function mint() public payable returns (bool success) {
         if (firstMinted[msg.sender]) {
             if (msg.value != mintPrice) {
                 revert BusinessCardBase__InvalidETHAmountSent();
             }
-
-            // _id = cards.length;
-            // cards.push(Card(_name, _email, _phone, _company, CardType.Personal, valueDesired));
-            // _safeMint(msg.sender, _id);
-            // cardToAddress[_id] = msg.sender;
-            // addressToCardCount[msg.sender]++;
-            // emit CardCreated(_id, _name, _email, _phone, _company, valueDesired);
-            addressToMintableAmount[msg.sender] += 1;
+            addressToDivisionToMintable[msg.sender][msg.sender] += 1;
         } else {
             if (msg.value != firstMintPrice) {
                 revert BusinessCardBase__InvalidETHAmountSent();
             }
             firstMinted[msg.sender] = true;
-
-            // for (uint256 i = 0; i < MAX_FIRST_MINT; i++) {
-            //     _id = cards.length;
-            //     cards.push(Card(_name, _email, _phone, _company, CardType.Personal, valueDesired));
-            //     _safeMint(msg.sender, _id);
-            //     icardToAddress[_id] = msg.sender;
-            //     iaddressToCardCount[msg.sender]++;
-            //     emit CardCreated(_id, _name, _email, _phone, _company, valueDesired);
-            // }
-            addressToMintableAmount[msg.sender] = 10;
+            addressToDivisionToMintable[msg.sender][msg.sender] = MAX_FIRST_MINT;
         }
+
+        return true;
+    }
+
+    function _mint(
+        string memory _name,
+        string memory _email,
+        string memory _phone,
+        address _company,
+        uint256 valueDesired
+    ) public virtual isMintable {
+        if (
+            bytes(_name).length * bytes(_email).length * bytes(_phone).length * valueDesired == 0 || _company == address(0)
+        ) {
+            revert BusinessCardBase__InvalidString();
+        }
+        string memory companyName = addressToCompanyName[_company];
+        Card memory card = Card({
+            name: _name,
+            email: _email,
+            phone: _phone,
+            company: companyName,
+            cardType: CardType.Personal,
+            valueDesired: valueDesired,
+            owner: msg.sender
+        });
+
+        cards.push(card);
+        uint256 _id = cards.length - 1;
+        addressToDivisionToMintable[msg.sender][msg.sender] -= 1;
+        _safeMint(msg.sender, _id);
+        emit CardCreated(_id, _name, _email, _phone, companyName, valueDesired, msg.sender);
+    }
+
+    function stake(string memory _company) public payable returns (bool success) {
+        if (msg.value != stakePrice) {
+            revert BusinessCardBase__NotStaked();
+        }
+
+        successfullyStaked[msg.sender] = true;
+        addressToCompanyName[msg.sender] = _company;
+
+        emit Stake(msg.sender, msg.value, _company);
+
+        return true;
+    }
+
+    function partyMint(
+        uint256 _employeeCount,
+        address[] memory employeeAddresses
+    ) external payable onlyStaked returns (bool success) {
+        if (_employeeCount > MAX_EMPLOYEES) {
+            revert BusinessCardBase__ExceededPeople();
+        }
+        if (msg.value != mintPrice * _employeeCount) {
+            revert BusinessCardBase__InvalidETHAmountSent();
+        }
+        if (_employeeCount != employeeAddresses.length) {
+            revert BusinessCardBase__InvalidArrayCount();
+        }
+
+        address companyAddress = msg.sender;
+        for (uint256 i = 0; i < _employeeCount; i++) {
+            addressToDivisionToMintable[employeeAddresses[i]][companyAddress] += 1;
+        }
+
+        emit PartyMint(msg.sender, employeeAddresses, uint32(_employeeCount));
 
         return true;
     }
@@ -100,32 +172,36 @@ contract BusinessCardBase is ERC721EnumerableUpgradeable {
         emit CardTransfer(_tokenId, _from, _to);
     }
 
-    function getFirstMintPrice() public view returns (uint256) {
+    function getFirstMintPrice() external view returns (uint256) {
         return firstMintPrice;
     }
 
-    function getMintPrice() public view returns (uint256) {
+    function getMintPrice() external view returns (uint256) {
         return mintPrice;
     }
 
-    function getCards() public view returns (Card[] memory) {
+    function getCards() external view returns (Card[] memory) {
         return cards;
     }
 
-    function getCard(uint256 _id) public view returns (Card memory) {
+    function getCard(uint256 _id) external view returns (Card memory) {
         return cards[_id];
     }
 
-    function getCardCount() public view returns (uint256) {
+    function getCardCount() external view returns (uint256) {
         return cards.length;
     }
 
-    function getCardCountByOwner(address _owner) public view returns (uint256) {
+    function getCardCountByOwner(address _owner) external view returns (uint256) {
         return balanceOf(_owner);
     }
 
-    function getCardByOwner(address _owner, uint256 _index) public view returns (Card memory) {
+    function getCardByOwner(address _owner, uint256 _index) external view returns (Card memory) {
         return cards[tokenOfOwnerByIndex(_owner, _index)];
+    }
+
+    function getCompanyName(address companyAddress) external view returns (string memory) {
+        return addressToCompanyName[companyAddress];
     }
 }
 
@@ -133,13 +209,14 @@ contract BusinessCardBase is ERC721EnumerableUpgradeable {
 // mint > 0?
 // internal i 제거
 // mintable
-
-// 권한 함수 분리
-// company명은 스테이킹 때 기입
 // event -> address
 // mintCard -> mint
+// 권한 함수 분리
+// Mint, transfer => convention
+// uint32 == uint256 when single
+// company명은 스테이킹 때 기입
+
+// permit
+
 // opcode x and +
 // keccak when comparing strings
-// uint32 == uint256 when single
-// Mint, transfer => convention
-// permit
